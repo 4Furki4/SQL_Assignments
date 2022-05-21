1d21b6c2969d968e1f6ba906f7c267c9bd425c20
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-05-21 08:24:30 +0300

    Ödev 6 yapıldı.

--- ODEV6.sql
+++ ODEV6.sql
@@ -1,5 +1,6 @@
 --1
-SELECT ROUND(AVG(rental_rate),2) FROM film; --2.98
+SELECT ROUND(AVG(rental_rate),2)
+FROM film; --2.98
 
 --2
 SELECT COUNT(*) FROM film
@@ -8,7 +9,6 @@ WHERE title ILIKE 'C%'; --92
 --3
 SELECT MAX(length) FROM film
 WHERE rental_rate=0.99; --184
-
 --4
 SELECT COUNT(DISTINCT replacement_cost) FROM film
 WHERE length >150; --21
