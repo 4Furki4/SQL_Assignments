0cc996770f8c9e4abdeab6155fc4c280a1a53196
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-05-23 22:50:59 +0300

    Ödev 11 yapıldı.

---
+++ ODEV11.sql
@@ -0,0 +1,22 @@
+--1
+(SELECT first_name FROM actor) 
+UNION
+(SELECT first_name FROM customer);
+
+--2
+(SELECT first_name FROM actor) 
+INTERSECT
+(SELECT first_name FROM customer);
+--3
+(SELECT first_name FROM actor) 
+EXCEPT
+(SELECT first_name FROM customer);
+--4.a
+(SELECT first_name FROM actor) 
+UNION ALL
+(SELECT first_name FROM customer);
+
+--4.b
+(SELECT first_name FROM actor) 
+EXCEPT ALL
+(SELECT first_name FROM customer);
